446e3797a6896873da45444c12b41ca85d63e845
Artiom Krits <artiom@proteantecs.com>
2025-03-21 10:25:46 +0200

    Added function placeholders for teammates in functions.py

---
+++ functions.py
@@ -0,0 +1,40 @@
+# functions.py
+
+def add_income(budget_data):
+    """
+    Collects income info from the user and adds it to the data structure.
+    :param budget_data: dict with 'balance' and 'transactions'
+    :return: updated budget_data
+    """
+    # To be implemented by a teammate
+    return budget_data
+
+
+def add_expense(budget_data):
+    """
+    Collects expense info from the user and adds it to the data structure.
+    :param budget_data: dict with 'balance' and 'transactions'
+    :return: updated budget_data
+    """
+    # To be implemented by a teammate
+    return budget_data
+
+
+def show_balance(budget_data):
+    """
+    Displays the current balance.
+    :param budget_data: dict with 'balance' and 'transactions'
+    :return: budget_data (unchanged)
+    """
+    # To be implemented by a teammate
+    return budget_data
+
+
+def show_transactions(budget_data):
+    """
+    Displays the full transaction history.
+    :param budget_data: dict with 'balance' and 'transactions'
+    :return: budget_data (unchanged)
+    """
+    # To be implemented by a teammate
+    return budget_data
